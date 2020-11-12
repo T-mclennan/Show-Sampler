fd6fe175e185e4cdc2c157e31a9af8f2cb218b41
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-11-11 22:50:02 -0800

    small changes to devtools syntax

--- src/store.js
+++ src/store.js
@@ -27,9 +27,10 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    typeof window !== 'undefined' &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+    ? a => a
+    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
